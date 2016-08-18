feb92ed03b7c0297f9292925f1fbf426dc940e12
rekarnar <rekarnar@gmail.com>
2016-08-18 15:10:50 +0545

    adding repo map lookup

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ var log = require('book');
 module.exports = function (app, models) {
 
     // ### POST /storyupdate/:repoPath
-    // Updates gitlab repo identified by `repoPath` according the event from pivotal 
+    // Updates gitlab repo identified by `repoPath` according the event from pivotal
     app.post(/^\/storyupdate(?:(?:\/(.*))?\/?)?/, function storyupdate (req, res) {
         // Check params
         if (!req.params || !req.params.length || req.params[0] === undefined) {
@@ -28,10 +28,30 @@ module.exports = function (app, models) {
         var repoPath = req.params[0],
             activity = req.body.activity;
 
+        console.log('Checking story:', activity.stories.story.name);
+        console.log('Orignal repo:', repoPath);
+
+        function updateRepo (title) {
+            var match = /\[(\w)\]/.exec(title)
+            // console.log('match', match);
+
+            if (match && match[1] && conf.repo_map[match[1]]) {
+                return conf.repo_map[match[1]]
+            } else {
+                return repoPath
+            }
+        }
+
+        if (conf.repo_map) {
+            repoPath = updateRepo(activity.stories.story.name)
+        }
+
+        console.log('Processed repo:', repoPath);
+
         // Handle event
         models.pivotal.handle(repoPath, activity, function (err, result) {
             if (err) res.send(400, err);
-            else res.send(200);            
+            else res.send(200);
         });
 
     });
